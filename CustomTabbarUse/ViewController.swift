b6926c7bba372182cd5a8b95004336af2cb16c73
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2023-01-23 15:58:39 +0300

    Initial Commit

---
+++ CustomTabbarUse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomTabbarUse
+//
+//  Created by Yılmaz Yağız Dokumacı on 23.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
